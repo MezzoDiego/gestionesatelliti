c9c025c3ef07aa54a90e72569d90d8ee79e9b439
MezzoDiego <diegomezzo90@gmail.com>
2022-11-15 21:16:18 +0100

    completamento della funzione del sito che consente di eliminare un satellite

--- src/main/java/it/prova/gestionesatelliti/web/controller/SatelliteController.java
+++ src/main/java/it/prova/gestionesatelliti/web/controller/SatelliteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -71,6 +72,21 @@ public class SatelliteController {
 		List<Satellite> results = satelliteService.findByExample(example);
 		model.addAttribute("satellite_list_attribute", results);
 		return "satellite/list";
+	}
+	
+	@GetMapping("/delete/{idSatellite}")
+	public String delete(@PathVariable(required = true) Long idSatellite, Model model) {
+		model.addAttribute("delete_satellite_attr", satelliteService.caricaSingoloElemento(idSatellite));
+		return "satellite/delete";
+	}
+	
+	@PostMapping("/saveDelete")
+	public String saveDelete(@RequestParam Long idSatellite, RedirectAttributes redirectAttrs) {
+
+		satelliteService.rimuovi(idSatellite);
+
+		redirectAttrs.addFlashAttribute("successMessage", "Operazione eseguita correttamente");
+		return "redirect:/satellite";
 	}
 
 }
